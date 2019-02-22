35bf2a9c7cc5bc8f57f441219709f4f34ee59a6d
Adrian Barszcz <adrian.barszcz@billennium.com>
2019-02-22 13:18:10 +0100

    checking all update

--- zad 3/examapp/src/app/tree/tree.component.ts
+++ zad 3/examapp/src/app/tree/tree.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChildren, ElementRef, AfterViewInit, QueryList } from '@angular/core';
 import { element } from '@angular/core/src/render3';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.scss']
 })
-export class TreeComponent implements OnInit {
+export class TreeComponent implements OnInit, AfterViewInit {
+
+  @ViewChildren('test', { read: ElementRef }) test: QueryList<ElementRef>;
 
   @Input() element: [];
   @Input() hidden: boolean;
+  checked: boolean;
   constructor() {
     this.element = [];
     this.hidden = false;
@@ -17,11 +21,16 @@ export class TreeComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+  }
+
   toggle() {
     this.hidden = !this.hidden;
   }
 
   afterClick() {
-
+    for (let i = 0; i < this.test.toArray().length; i++) {
+    this.test.toArray()[i].nativeElement['checked'] = true;
+    }
   }
 }
